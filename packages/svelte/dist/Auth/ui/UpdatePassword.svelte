665b0def7c118090bfd098ab050a15fc21cf99b0
Alberto <acubeddu87@gmail.com>
2024-05-11 10:23:55 +1000

    dist# Please enter the commit message for your changes. Lines starting

---
+++ packages/svelte/dist/Auth/ui/UpdatePassword.svelte
@@ -0,0 +1,20 @@
+<script>import Auth from '../Auth.svelte';
+export let supabaseClient;
+export let redirectTo = undefined;
+export let showLinks = false;
+export let appearance = {};
+export let theme = 'default';
+export let passwordLimit = false;
+export let localization = {};
+</script>
+
+<Auth
+	view="update_password"
+	{showLinks}
+	{supabaseClient}
+	{redirectTo}
+	{appearance}
+	{theme}
+	{passwordLimit}
+	{localization}
+/>
